e1e9de73d9bcfb4cb7182439719f840b7c3d5099
reginal <wengzylangs@gmail.com>
2025-07-24 16:41:34 +0100

    money

--- task3.py
+++ task3.py
@@ -10,3 +10,10 @@ money = [1000, 1200, 800, 1500, 1100]
 
 → Perform the corrections and computations, and print all results.
 """
+
+money = [1000, 1200, 800, 1500, 1100]
+total_money=money[0]+money[1]+money[2]+money[3]+money[4]
+print(total_money)
+
+money[2]=1000
+print(money)
